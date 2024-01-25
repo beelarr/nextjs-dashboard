b28b180b4d2c0feef0bb99fa3dd6b480951b66d0
Bryon Larrance <blarrance@sertasimmons.com>
2024-01-25 14:40:49 -0500

    feat: add chap 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,7 +22,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 13000));
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -39,7 +39,7 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
-    await new Promise((resolve) => setTimeout(resolve, 13000));
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
