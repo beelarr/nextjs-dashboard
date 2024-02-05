97644c0a0ee177e2423d34a16fe05b21f4bba69d
Bryon Larrance <blarrance@sertasimmons.com>
2024-02-05 14:14:48 -0600

    feat: add lint command and form validation for creating

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,38 +3,71 @@
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
-type FormSchema = {
-  id: string;
-  customerId: string;
-  amount: string;
-  status: 'pending' | 'paid';
-  date: string;
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
 };
 
-type Invoice = Omit<FormSchema, 'id' | 'date'>;
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
 
-export const createInvoice = async (formData: FormData) => {
-  try {
-    const { customerId, amount, status } = Object.fromEntries(
-      formData.entries(),
-    ) as Invoice;
-    const amountInCents = Number(amount) * 100;
-    const date = new Date().toISOString().split('T')[0];
-
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`;
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-  } catch (error) {
-    // Send to monitoring
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
     return {
-      message: 'Database Error: Failed to create Invoice',
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
-};
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // Insert data into the database
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // If a database error occurs, return a more specific error.
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  } finally {
+    // Revalidate the cache for the invoices page and redirect the user.
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+}
 
 export const updateInvoice = async (id: string, formData: FormData) => {
   try {
